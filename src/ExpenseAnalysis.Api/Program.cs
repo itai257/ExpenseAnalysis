bb140379ddc194f3e0fc16fb6207784febce25c4
Itay Malka <itai257@gmail.com>
2025-03-30 01:33:39 +0300

    some cleanup

--- src/ExpenseAnalysis.Api/Program.cs
+++ src/ExpenseAnalysis.Api/Program.cs
@@ -22,13 +22,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.RegisterEndpoints();
 
-
 app.Run();
 
 namespace ExpenseAnalysis.Api
 {
-    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
 }
